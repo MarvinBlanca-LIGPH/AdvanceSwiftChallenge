d4dcd725d6ae0c5a6103a49f78c2e26e27125685
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-03-08 10:30:12 +0800

    Completed Project

---
+++ Tipsy/Model/Tip.swift
@@ -0,0 +1,15 @@
+//
+//  Tip.swift
+//  Tipsy
+//
+//  Created by Mark Marvin Blanca on 3/5/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Tip {
+    var amount: Float
+    var percent: Double
+    var splitCount: Int
+}
